405883afa8a05b89c9b2f22836ced7596bc85e6b
Kaitlyn Diaz <katediaz011@gmail.com>
2022-09-28 15:18:29 -0400

    changing cell css And cell.js

--- force-app/main/default/lwc/cell/cell.js
+++ force-app/main/default/lwc/cell/cell.js
@@ -9,9 +9,10 @@ export default class Cell extends NavigationMixin(LightningElement) {
     @api rowId;
     @api getColorSetting;
 
-   
 
-    connectedCallback(){ var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
+
+    connectedCallback() {
+        var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
         // var parsedCellColor = JSON.parse(JSON.stringify(this.getColorSetting));
 
         // console.log(parsedCell)
@@ -36,115 +37,141 @@ export default class Cell extends NavigationMixin(LightningElement) {
        type on the cell HTML */
     get isText() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.fieldName === 'Name') return false;
-        if(parsedCell.type === 'string' || parsedCell.type === 'picklist' || parsedCell.type === 'textarea' || parsedCell.type === 'id')  return true;
+        if (parsedCell.fieldName === 'Name') return false;
+        if (parsedCell.type === 'string' || parsedCell.type === 'picklist' || parsedCell.type === 'textarea' || parsedCell.type === 'id') return true;
         return false;
     }
 
     get isPhone() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'phone') return true;
+        if (parsedCell.type === 'phone') return true;
         return false;
     }
 
     get isEmail() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'email') return true;
+        if (parsedCell.type === 'email') return true;
         return false;
     }
 
     get isDate() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'date') return true;
+        if (parsedCell.type === 'date') return true;
         return false;
     }
 
     get isDateTime() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'datetime') return true;
+        if (parsedCell.type === 'datetime') return true;
         return false;
     }
 
     get isAddress() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'address') return true;
+        if (parsedCell.type === 'address') return true;
         return false;
     }
 
     get isPercent() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'percent') return true;
-       
+        if (parsedCell.type === 'percent') return true;
+
         return false;
     }
 
     get isDecimal() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'decimal' || parsedCell.type === 'double') return true;
+        if (parsedCell.type === 'decimal' || parsedCell.type === 'double') return true;
         return false;
     }
 
     get isCurrency() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'currency') return true;
+        if (parsedCell.type === 'currency') return true;
         return false;
     }
 
     get isUrl() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'url') return true;
+        if (parsedCell.type === 'url') return true;
         return false;
     }
 
     get isBool() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'boolean') return true;
+        if (parsedCell.type === 'boolean') return true;
         return false;
     }
 
     get isInteger() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.type === 'number') return true;
+        if (parsedCell.type === 'number') return true;
         return false;
     }
 
     get isReference() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
-        if(parsedCell.fieldName === 'Name') return true;
+        if (parsedCell.fieldName === 'Name') return true;
         return false;
     }
 
 
 
-    get cellColor (){
+    get cellColor() {
         var parsedCell = JSON.parse(JSON.stringify(this.cellInfo));
         var parsedColorSetting = JSON.parse(JSON.stringify(this.getColorSetting));
-        console.log(JSON.parse(JSON.stringify(this.getColorSetting)));
+        console.log('parsed color setting', parsedColorSetting);
+       
+        for (let i = 0; i < parsedColorSetting[0].cellColorSettings[i].condition.length; i++) {
 
-        if (parsedCell.data > this.getColorSetting[0].cellColorSettings[0].dataValue && this.getColorSetting[0].fieldAPIName == 'Temperature__c') {
-            return this.getColorSetting.cellColorSettings[0].color;
-        } 
-        if (parsedCell.data > this.getColorSetting[0].cellColorSettings[0].dataValue && this.getColorSetting[0].fieldAPIName == 'Temperature__c') {
-            return this.getColorSetting.cellColorSettings[0].color;
-        } 
-        if (parsedCell.data > this.getColorSetting.cellColorSettings[0].dataValue && this.getColorSetting.fieldAPIName == 'Humidity__c') {
-            return this.getColorSetting.cellColorSettings[0].color;
-        } 
-        if (parsedCell.fieldName == 'Humidity__c' && parsedCell.data < 80) {
-            return 'coloryellow';
+            switch (parsedColorSetting[0].cellColorSettings[i].condition) {
+                case 'greater than':
+                    if (parsedCell.data > parsedColorSetting[0].cellColorSettings[0].dataValue) {
+                        if (parsedColorSetting[0].fieldAPIName === 'Temperature__c') {
+                            // returns red
+                            return parsedColorSetting[0].cellColorSettings[0].color;
+                        }
+                        else {
+                            return 'colorblack'
+                        }
+                    }
+                    break;
+                case 'less than':
+                    if (parsedCell.data < parsedColorSetting[0].cellColorSettings[1].dataValue) {
+                        if (parsedColorSetting[0].fieldAPIName === "Temperature__c") {
+                            // returns yellow
+                            return parsedColorSetting[0].cellColorSettings[1].color;
+                        }
+                        else {
+                            return 'colorblack'
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
-      
-        
-        // if (parsedCell.type === 'decimal' || parsedCell.type === 'double' && parsedCell.fieldName == 'Wind_MPH__c' && parsedCell.data < 5) {
-        //     return 'radius';
-        // } 
-        // if (parsedCell.type === 'decimal' || parsedCell.type === 'double' && parsedCell.fieldName == 'Wind_MPH__c' && parsedCell.data > 5) {
-        //     return 'radiusyellow';
-        // }
-        // else {
-            // return 'colorblack';
-        // };
+
     };
+
+    // if (parsedCell.data > this.getColorSetting.cellColorSettings[0].dataValue && this.getColorSetting.fieldAPIName == 'Humidity__c') {
+    //     return this.getColorSetting.cellColorSettings[0].color;
+    // } 
+    // if (parsedCell.fieldName == 'Humidity__c' && parsedCell.data < 80) {
+    //     return 'coloryellow';
+    // }
+
+
+    // if (parsedCell.type === 'decimal' || parsedCell.type === 'double' && parsedCell.fieldName == 'Wind_MPH__c' && parsedCell.data < 5) {
+    //     return 'radius';
+    // } 
+    // if (parsedCell.type === 'decimal' || parsedCell.type === 'double' && parsedCell.fieldName == 'Wind_MPH__c' && parsedCell.data > 5) {
+    //     return 'radiusyellow';
+    // }
+    // else {
+    // return 'colorblack';
+    // };
+
 
 
 }
